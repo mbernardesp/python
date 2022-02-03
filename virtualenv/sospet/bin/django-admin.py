f1c7c52c36eaca67188d50c1c2efe5cfa198cc7c
Marcelo <marcelobernardespinto@gmail.com>
2022-02-03 11:14:29 -0300

    change environment

--- virtualenv/sospet/bin/django-admin.py
+++ virtualenv/sospet/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/marcelo/Documents/projects/virtualenv/sospet/bin/python3
+#!/workspace/projects/python/virtualenv/sospet/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
